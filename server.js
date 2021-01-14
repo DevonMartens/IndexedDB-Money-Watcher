013d6ce43edbb90236deec156de7b0a664ff250b
Devon Martens <devonmartens@Devons-MacBook-Pro-2.local>
2021-01-14 10:24:13 -0500

    set up URI

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pwa", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: true
+    useFindAndModify: false
   }
 );
 
