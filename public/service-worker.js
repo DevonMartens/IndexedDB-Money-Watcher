5d3a00d5aa7ed2c91206d01a54e7b9455499b7df
Devon Martens <devonmartens@Devons-MacBook-Pro-2.local>
2021-02-02 09:26:04 -0500

    testing service worker changes

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,8 +4,8 @@ const FILES_TO_CACHE = [
     "/styles.css",
     "/db.js",
     "/manifest.webmanifest",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
+    "./icons/icon-192x192.png",
+    "./icons/icon-512x512.png"
 ];
 const STATIC_CACHE = "static-cache-v1";
 const RUNTIME_CACHE = "runtime-cache";
